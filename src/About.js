7c7aad1ada6c2317148a7a903a3de1deb4166a62
jerryjohns <jerry.johns@student.moringaschool.com>
2022-09-12 14:29:42 +0300

    styles

--- src/About.js
+++ src/About.js
@@ -1,17 +1,20 @@
 import React from 'react'
-
+import Card from 'react-bootstrap/Card';
 const About = () => {
   return (
-    <div>
-    <h1><strong>About us</strong></h1>
 
+  <div className='lolo'>
+<Card>
+<h1><strong>ABOUT US</strong></h1>
 <p> Mamboleo spa offers goood and best service to you. We do online and offline . You can make an online booking and our agents will be there for you.
   We carter all services offered  
-
+  <p> Come one come all. Our doors are open for you </p>
+<img src='https://img.freepik.com/premium-photo/spa-concept-with-zen-basalt-stones_488220-20362.jpg?w=2000'
+></img>
 </p>
 
-<p> Come one come all. Our doors are open for you </p>
-    </div>
+</Card>
+  </div>
   )
 }
 
